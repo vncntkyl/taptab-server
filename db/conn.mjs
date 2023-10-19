67049ff211e3c73f864f008f2ca61278316f81d1
vncntkyl <kyle.rinoza2009@gmail.com>
2023-10-19 13:01:58 +0800

    updated conn string

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const connectionString =
-  "mongodb+srv://kylerinoza:tqh0vgJbTUs1TBma@cluster-0.2fhszl4.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://kylerinoza:tqh0vgJbTUs1TBma@cluster-0.2fhszl4.mongodb.net/?retryWrites=true&w=majority&tls=true";
 const client = new MongoClient(connectionString);
 
 let conn;
