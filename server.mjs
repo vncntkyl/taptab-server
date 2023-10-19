aaf47346ee2c788bb86db1ecdf2f58af3ca1847b
Vincent <83127251+vncntkyl@users.noreply.github.com>
2023-10-19 10:01:57 +0800

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/records.mjs";
+import users from "./routes/users.mjs";
+import storage from "./routes/storage.mjs";
+import staticAds from "./routes/staticAds.mjs";
+import surveys from "./routes/surveys.mjs";
+import planners from "./routes/planner.mjs";
+import players from "./routes/players.mjs";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/record", records);
+app.use("/users", users);
+app.use("/storage", storage);
+app.use("/staticAds", staticAds);
+app.use("/surveys", surveys);
+app.use("/planner", planners);
+app.use("/players", players);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
